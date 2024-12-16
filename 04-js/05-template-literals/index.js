34c68e00b47e08acc01b6dbc837dd1c1ce77bc54
Paula Rodrigues <ipaulaa.dev@gmail.com>
2024-12-16 08:57:10 -0300

    :books: lectures → strings

---
+++ 04-js/05-template-literals/index.js
@@ -0,0 +1,7 @@
+const name = "Paula";
+const birthYear = 1996;
+
+const CURRENT_YEAR = 2024;
+
+const intro = `My name is ${name} and I'm ${CURRENT_YEAR - birthYear} years old.`;
+console.log(intro);
