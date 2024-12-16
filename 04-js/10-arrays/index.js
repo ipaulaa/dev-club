45b71b2542e205c92fff67fa8f37a9aa7ba0b75b
Paula Rodrigues <ipaulaa.dev@gmail.com>
2024-12-16 12:18:24 -0300

    :books: lectures → arrays

---
+++ 04-js/10-arrays/index.js
@@ -0,0 +1,24 @@
+const miscArray = [3.14159, "Hello World!", { name: "Paula", age: 28 }];
+console.log(miscArray);
+
+const users = [
+  { name: "Charles", age: 52 },
+  { name: "Paula", age: 28 }
+];
+
+console.log(users);
+
+const primeNumbers = [2, 3, 5, 7, 11, 13, 17, 23, 29];
+
+console.log(primeNumbers[10]);
+console.log(primeNumbers[9]);
+console.log(primeNumbers[0]);
+
+const names = ["Anne", "Bea", "Charles"];
+console.log(names);
+
+names[2] = "Chuck";
+console.log(names);
+
+users[0].name = "Chuck";
+console.log(users);
