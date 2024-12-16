524cae6af12430463bba3724507143aa9edf9903
Paula Rodrigues <ipaulaa.dev@gmail.com>
2024-12-16 12:43:10 -0300

    :books: lectures → if-else

---
+++ 04-js/11-if-else/index.js
@@ -0,0 +1,29 @@
+let grade = 9;
+const cutoffGrade = 6;
+
+console.log(`GRADE: ${grade}`);
+console.log("... if ...");
+if (grade > cutoffGrade) {
+  console.log("Pass 🎉");
+}
+
+console.log("... if-else ...");
+if (grade > cutoffGrade) {
+  console.log("Pass 🎉");
+} else {
+  console.log("Fail 😭");
+}
+
+grade = 3;
+console.log(`GRADE: ${grade}`);
+console.log("... if ...");
+if (grade > cutoffGrade) {
+  console.log("Pass 🎉");
+}
+
+console.log("... if-else ...");
+if (grade > cutoffGrade) {
+  console.log("Pass 🎉");
+} else {
+  console.log("Fail 😭");
+}
